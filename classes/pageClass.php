3dff6d12af8b9ad7353d2f09f712125e4c7e9982
Rafal Dudek <dudek.raf@gmail.com>
2013-07-03 22:08:44 +0100

    meta and menu page methods

--- classes/pageClass.php
+++ classes/pageClass.php
@@ -4,19 +4,69 @@ class page{
 	private $css;
 	private $body;
 	private $title;
+	private $meta;
 	
 	public function __construct($css='',$js='',$title='',$body=''){
 		$this->js=$js;
 		$this->css=$css;
 		$this->title=$title;
 		$this->body=$body;
+		$this->meta=array();
 		
 	}
 	
+	//Adds meta tags to the page
+	//arr - array for metadata e.g.('charset'=>'utf-8')
+	public function addMeta($arr){
+		$this->meta[]=$arr;
+	}
+	
+	//Ads html code to page body element
+	public function addBody($html){
+		$this->body.=$html;
+		return $this->body;
+	}
+	
+	//Generates and returns menu code based on array
+	public function generateMenu($array,$class=''){
+		$h='<ul';
+		if($class!=''){
+			$h.=' class="'.$class.'"';
+		}
+		$h.='>';
+		foreach($array as $tmp){
+			$h.='<li';
+			if(isset($tmp['class']) && $tmp['class']!=''){
+				$h.=' class="'.$tmp['class'].'"';
+			}
+			$h.='>';
+			if(isset($tmp['href']) && $tmp['href']!=''){
+				$h.='<a href="'.$tmp['href'].'">'.$tmp['text'].'</a>';
+			}else{
+				$h.=$tmp['text'];	
+				if(isset($tmp['children']) && is_array($tmp['children'])){
+					$h.=$this->generateMenu($tmp['children']);
+				}
+			}
+			$h.='</li>';
+		}
+		$h.='</ul>';
+		return $h;
+	}
+	
+	//Prints page
 	public function printPage(){
 		$h='<!DOCTYPE html><html>';
 		$h.='<head>';
-		$h.='<meta charset="utf8">';
+		if(count($this->meta)>0){
+			foreach($this->meta as $tmp){
+				$h.='<meta';	
+				foreach($tmp as $key=>$value){
+					$h.=' '.$key.'="'.$value.'"';
+				}
+				$h.='>';
+			}
+		}
 		$h.='<title>'.$this->title.'</title>';
 		if(is_array($this->css) && count($this->css)>0){
 			foreach($this->css as $css){
--- tests/test.php
+++ tests/test.php
@@ -10,25 +10,61 @@
 	
 	$page=new page();
 	$test->m='Empty page print';
-	$test->equal('<!DOCTYPE html><html><head><meta charset="utf8"><title></title></head><body></body></html>',$page->printPage());
+	$test->equal('<!DOCTYPE html><html><head><title></title></head><body></body></html>',$page->printPage());
 	
 	
 	$page=new page('','','Page title','');
 	$test->m='Title page print';
-	$test->equal('<!DOCTYPE html><html><head><meta charset="utf8"><title>Page title</title></head><body></body></html>',$page->printPage());
+	$test->equal('<!DOCTYPE html><html><head><title>Page title</title></head><body></body></html>',$page->printPage());
 	
 	
 	$page=new page('','','Page title','Page body');
 	$test->m='Body page print';
-	$test->equal('<!DOCTYPE html><html><head><meta charset="utf8"><title>Page title</title></head><body>Page body</body></html>',$page->printPage());
+	$test->equal('<!DOCTYPE html><html><head><title>Page title</title></head><body>Page body</body></html>',$page->printPage());
 	
 	$page=new page(array('css1.css','css2.css'),'','Page title','Page body');
 	$test->m='CSS page print';
-	$test->equal('<!DOCTYPE html><html><head><meta charset="utf8"><title>Page title</title><link rel="stylesheet" type="text/css" href="css1.css"><link rel="stylesheet" type="text/css" href="css2.css"></head><body>Page body</body></html>',$page->printPage());
+	$test->equal('<!DOCTYPE html><html><head><title>Page title</title><link rel="stylesheet" type="text/css" href="css1.css"><link rel="stylesheet" type="text/css" href="css2.css"></head><body>Page body</body></html>',$page->printPage());
 	
 	$page=new page('',array('js1.js','js2.js'),'Page title','Page body');
 	$test->m='JS page print';
-	$test->equal('<!DOCTYPE html><html><head><meta charset="utf8"><title>Page title</title></head><body>Page body<script type="text/javascript" src="js1.js"></script><script type="text/javascript" src="js2.js"></script></body></html>',$page->printPage());
+	$test->equal('<!DOCTYPE html><html><head><title>Page title</title></head><body>Page body<script type="text/javascript" src="js1.js"></script><script type="text/javascript" src="js2.js"></script></body></html>',$page->printPage());
+	
+	$page=new page('','','Page title','');
+	$page->addMeta(array('charset'=>'utf-8'));
+	$test->m='Single meta data ';
+	$test->equal('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Page title</title></head><body></body></html>',$page->printPage());
+	
+	$page=new page('','','Page title','');
+	$page->addMeta(array('charset'=>'utf-8'));
+	$page->addMeta(array('a'=>'aa','b'=>'bb'));
+	$test->m='Multiple meta data';
+	$test->equal('<!DOCTYPE html><html><head><meta charset="utf-8"><meta a="aa" b="bb"><title>Page title</title></head><body></body></html>',$page->printPage());
+	
+	
+	$page=new page();
+	$page->addBody('Hello');
+	$test->m='Add body function';
+	$test->equal('<!DOCTYPE html><html><head><title></title></head><body>Hello</body></html>',$page->printPage());
+	
+	
+	$page=new page();
+	$arr=array();
+	$arr[]=array('text'=>'Item 1','href'=>'href 1','class'=>'a');
+	$arr[]=array('text'=>'Item 2','href'=>'href 2','class'=>'b');
+	$test->m='Generate menu simple';
+	$test->equal('<ul class="menu"><li class="a"><a href="href 1">Item 1</a></li><li class="b"><a href="href 2">Item 2</a></li></ul>',$page->generateMenu($arr,'menu'));
+	
+	$page=new page();
+	$ch=array();
+	$ch[]=array('text'=>'Item 1','href'=>'href 1','class'=>'a');
+	$ch[]=array('text'=>'Item 2','href'=>'href 2','class'=>'b');
+	$arr=array();
+	$arr[]=array('text'=>'Item 1','href'=>'href 1','class'=>'a');
+	$arr[]=array('text'=>'Item 2','children'=>$ch);
+	$test->m='Generate menu multilevel';
+	$test->equal('<ul class="menu"><li class="a"><a href="href 1">Item 1</a></li><li>Item 2<ul><li class="a"><a href="href 1">Item 1</a></li><li class="b"><a href="href 2">Item 2</a></li></ul></li></ul>',$page->generateMenu($arr,'menu'));
+	
 	
 	
 ?>
